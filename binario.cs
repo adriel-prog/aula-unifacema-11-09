5d07f3dc0829f25d0b224c8d5020d8b20b96c39a
Adriel victor <127162965+adriel-prog@users.noreply.github.com>
2023-09-11 21:04:35 -0300

    Add files via upload

---
+++ binario.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Digite um número decimal: ");
+int numerodecimal = int.Parse(Console.ReadLine());
+string numerobinario = decimalparabinario(numerodecimal);
+Console.WriteLine($"O numero binario equivale a {numerobinario}");
+
+static string decimalparabinario(int numerodecimal)
+{
+    if (numerodecimal == 0)
+        return "0";
+    string binario = "";
+    while (numerodecimal > 0)
+    {
+        int i = numerodecimal % 2;
+        binario = i + binario;
+        numerodecimal /= 2;
+
+    }
+    return binario;
+}
+
+ 
